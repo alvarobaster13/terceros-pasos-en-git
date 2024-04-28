d1eb557f67019c7c07bb9dfd608138c5eea6b78c
marcos <marcosnuero11@gmail.com>
2024-04-28 18:23:44 +0200

    commit y push al repositorio de Alvaro

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hola");
+        Scanner scanner = new Scanner(System.in);
+        int hola = scanner.nextInt();
+        System.out.println(hola);
     }
 }
